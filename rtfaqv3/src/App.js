2b8f1379849b796be140f0bf4a98e9146b221f02
amacken3 <aengus.2000@gmail.com>
2024-09-09 13:01:37 -0400

    linked homepage

--- rtfaqv3/src/App.js
+++ rtfaqv3/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/home" elemen={<Home/>} />
       </Routes>
     </Router>
   );
